4c606341c3925b739990b7db00eb15cf4dd5eb90
Chandan Kumar Shaw <66195931+i-am-Chandan-Shaw@users.noreply.github.com>
2020-07-07 17:30:36 +0530

    Add files via upload
    Complete project without node modules

---
+++ build/precache-manifest.a58cd2be22297a75999af2f331451a62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4262de33f8ba542474004188dd87fcd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd158ab17a105aef108",
+    "url": "/static/js/2.fb6728fe.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.fb6728fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2de70b95f0d2a243a6",
+    "url": "/static/js/main.a28eeec8.chunk.js"
+  },
+  {
+    "revision": "272b2118ce1a3384d9bd",
+    "url": "/static/js/runtime-main.342c7561.js"
+  }
+]);
